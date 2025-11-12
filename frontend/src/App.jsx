59ec8f4202f990a271652568cb25ced90f5f1fc3
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-11-12 23:18:05 +0900

    Chore : 코드 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import Mypage from "./pages/Mypage";
+// import LoginPage from "./pages/LoginPage";
+// import RegisterPage from "./pages/RegisterPage";
+// import Mypage from "./pages/Mypage";
 import Regions from "./pages/Regions.jsx";
 
 
@@ -9,9 +9,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/mypage" element={<Mypage />} />
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
+        {/* <Route path="/mypage" element={<Mypage />} /> */}
         <Route path="/regions" element={<Regions />} />
       </Routes>
     </Router>
--- frontend/src/pages/Regions.jsx
+++ frontend/src/pages/Regions.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./Regions.css"
 
+const navigate = useNavigate()
 
 
 function Regions() {
