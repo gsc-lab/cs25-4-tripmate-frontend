093c513516def0a8416cc7fae391f81c38b64f89
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-11-13 22:43:36 +0900

    Fix: axios 수정

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Box, TextField, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import AuthLayout from "../components/AuthLayout.jsx";
+import axios from "axios";
 
 function LoginPage() {
 
