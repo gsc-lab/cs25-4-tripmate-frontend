1af9401e1bd645a6ce84937d2179468c1a304183
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-11-16 13:58:06 +0900

    Chore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+import Register from "./pages/Register";
 import Mypage from "./pages/Mypage";
 // import Regions from "./pages/Regions";
 
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/mypage" element={<Mypage />} />
         {/* <Route path="/regions" element={<Regions />} /> */}
       </Routes>
